1b493e03271c473e17d33d5bdeb0e85f49ce3c4f
George Grant <dontsellmydata@sackowoe.net>
2022-02-03 12:45:41 +0000

    Initial commit

---
+++ aci.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env/python3
+import requests
+import json
+import sys
+from staging_ra_automator import nl
+from time import sleep
+
+APIC_ADDRESS = "192.168.104.10"
+
+
+def get_ports():
+
+    while True:
+
+        try:
+            port_list = []
+            ports = [
+                int(port)
+                for port in input(
+                    """
+Please enter the FEX switch port numbers of where you have patched in devices:
+
+>>> """
+                ).split()
+            ]
+
+            for x in ports:
+                if x in range(1, 49):
+                    port_list.append(x)
+                elif x == 0:
+                    port_list = 0
+                    sys.exit(1)
+                else:
+                    sys.exit(1)
+
+            return port_list
+        except:
+            if port_list == 0:
+                print("\n[-] Goodbye...\n")
+                sys.exit(0)
+            else:
+                print(
+                    "\n[-] ERROR: Please enter port numbers between 1 and 48 or enter 0 to quit.\n"
+                )
+
+
+def get_APIC_password():
+    password = input(
+        """
+Please enter the APIC admin password: 
+    
+>>> """
+    )
+    return password
+
+
+def select_ports(token, ports):
+    portList = ports
+
+    uri = f"https://{APIC_ADDRESS}/api/node/mo/uni/tn-HighPoint-Customers/ap-Staging/epg-Customer-Access-EPG.json"
+
+    payload = {
+        "fvRsPathAtt": {
+            "attributes": {
+                "dn": "uni/tn-HighPoint-Customers/ap-Staging/epg-Customer-Access-EPG/rspathAtt-[topology/pod-1/paths-101/extpaths-101/pathep-[eth1/x]]",
+                "encap": "vlan-2022",
+                "mode": "native",
+                "tDn": "topology/pod-1/paths-101/extpaths-101/pathep-[eth1/x]",
+                "rn": "rspathAtt-[topology/pod-1/paths-101/extpaths-101/pathep-[eth1/x]]",
+                "status": "created",
+            },
+            "children": [],
+        }
+    }
+
+    dn = payload["fvRsPathAtt"]["attributes"]["dn"]
+    rn = payload["fvRsPathAtt"]["attributes"]["rn"]
+    tDn = payload["fvRsPathAtt"]["attributes"]["tDn"]
+
+    for port in portList:
+        new_dn = dn.replace("eth1/x", "eth1/{}").format(port)
+        new_rn = rn.replace("eth1/x", "eth1/{}").format(port)
+        new_tDn = tDn.replace("eth1/x", "eth1/{}").format(port)
+        payload["fvRsPathAtt"]["attributes"]["dn"] = new_dn
+        payload["fvRsPathAtt"]["attributes"]["rn"] = new_rn
+        payload["fvRsPathAtt"]["attributes"]["tDn"] = new_tDn
+
+        headers = {
+            "Cookie": f"APIC-Cookie={token}",
+        }
+
+        session = requests.Session()
+
+        requests.packages.urllib3.disable_warnings()
+        response = session.post(
+            uri, data=json.dumps(payload), headers=headers, verify=False
+        ).json()
+
+        if response["totalCount"] == "0":
+            nl()
+            print(f"[+] Port eth1/{str(port)} enabled successfully!")
+        else:
+            nl()
+            print(f"[-] Error enabling port eth1/{str(port)}")
+    nl()        
+    print("[+] Taking you back to the main menu...")
+    sleep(3)
+
+
+def get_token(apic_password):
+    apic_user = "admin"
+    apic_password = apic_password
+
+    headers = {"content-type": "application/json", "cache-control": "no-cache"}
+
+    uri = "https://{0}/api/aaaLogin.json".format(APIC_ADDRESS)
+
+    payload = {"aaaUser": {"attributes": {"name": apic_user, "pwd": apic_password}}}
+
+    session = requests.Session()
+
+    requests.packages.urllib3.disable_warnings()
+    response = session.post(
+        uri, data=json.dumps(payload), headers=headers, verify=False
+    ).json()
+
+    try:
+        key = list(response["imdata"][0]["aaaLogin"]["attributes"].keys())
+
+        if key[0] == "token":
+
+            token = response["imdata"][0]["aaaLogin"]["attributes"]["token"]
+            return token
+
+    except:
+        print("[-] Error: APIC password is wrong.")
+        return 0
+
+
+def aci():
+    # get list of ports the engineer has patched devices into on the FEX switch.
+    ports = get_ports()
+    # get the APIC admin password.
+    APIC_password = get_APIC_password()
+    # login to the APIC and get authenticated session cookie.
+    token = get_token(APIC_password)
+    if token == 0:
+        aci()
+    else:
+        # post the list of ports to the HPS Customer-Access-EPG.
+        select_ports(token, ports)
---
+++ staging_ra_automator.py
@@ -0,0 +1,283 @@
+#!/usr/bin/env/python3
+import pyfiglet
+import sys
+from aci import *
+from time import sleep
+from password_strength import PasswordPolicy
+from ldap3 import Server, Connection, NTLM, ALL
+from ldap3.extend.microsoft.addMembersToGroups import (
+    ad_add_members_to_groups as addUsersInGroups,
+)
+
+
+def password_checker(password):
+    policy = PasswordPolicy.from_names(
+        length=10,  # min length: 8
+        uppercase=1,  # need min. 2 uppercase letters
+        numbers=1,  # need min. 2 digits
+        special=1,  # need min. 2 special characters
+        nonletters=1,  # need min. 2 non-letter characters (digits, specials, anything)
+    )
+    tested_password = policy.test(password)
+
+    return tested_password
+
+
+def nl():
+    print("\n")
+
+
+def menu():
+    banner()
+    nl()
+    choice = input(
+        """
+1: Create Active Directory user
+2: Configure networking for staged devices
+0: Quit
+
+>>> """
+    )
+    if choice == "1":
+        create_user()
+        menu()
+    elif choice == "2":
+        aci()
+        menu()
+    elif choice == "0":
+        print("Goodbye...")
+        sys.exit(0)
+    else:
+        print("[-] You must only select either 1 or 2")
+        sleep(2)
+        nl()
+        nl()
+        menu()
+
+
+def banner():
+    ascii_banner = pyfiglet.figlet_format("HPS Remote Access Automator v1.0")
+    print(ascii_banner)
+    print("Created by George Grant 2022")
+    print("-" * 80)
+
+
+def get_args():
+
+    adpass = input(
+        """
+Please enter the AD Server Administrator password:
+
+>>> """
+    )
+
+    firstname = input(
+        """
+The first name of the person requiring access:
+
+>>> """
+    )
+
+    lastname = input(
+        """
+The last name of the person requiring access:
+
+>>> """
+    )
+
+    while True:
+        try:
+            user_password = input(
+                """
+The new COMPLEX password for the person requiring access:
+
+>>> """
+            )
+
+            checked_password = password_checker(user_password)
+
+            if (
+                checked_password == []
+                and user_password.lower().find(firstname.lower()) == -1
+                and user_password.lower().find(lastname.lower()) == -1
+            ):
+                return adpass, firstname, lastname, user_password
+            else:
+                print(
+                    """
+[-] ERROR: Password must be:
+
+[-] Longer than 10 characters
+[-] Contain at least 1 uppercase character
+[-] Contain at least 1 number
+[-] Contain at least 1 special character
+[-] NOT contain the users first or last name
+            """
+                )
+
+        except:
+            sys.exit("[-] ERROR: quitting...")
+
+
+def connection(adpassword):
+    # connect
+    server = Server("W2K16-SRV.hps.lab", use_ssl=True, get_info=ALL)
+    connect = Connection(
+        server,
+        user="HPSLAB\Administrator",
+        password=adpassword,
+        authentication=NTLM,
+        auto_referrals=False,
+    )
+    if not connect.bind():
+
+        return 0
+    nl()
+    print("[+] Right, we're connected to the AD server! Let's create that user...\n")
+    return connect
+
+
+def create_user():
+    # get input from the user
+    args = get_args()
+
+    try:
+        connected = connection(args[0])
+        if connected == 0:
+            nl()
+            print(
+                "[-] Failed to connect to AD Server.\n[-] Check the password.\n[-] Are you connected to the HPSLAB Wireless or CATO VPN?"
+            )
+            nl()
+            print("[-] Taking you back to the main menu...")
+            sleep(3)
+            return
+
+    except:
+        sys.exit("[-] ERROR: quitting...")
+
+    # creating the new users distinguised name
+    userdn = "cn={},cn=Users,dc=hps,dc=lab".format(
+        args[1].capitalize() + " " + args[2].capitalize()
+    )
+    # location of the restricted Global Protect Palo VPN group
+    customer_access_groupdn = "cn=GlobalProtect-Customer-Access,cn=Users,dc=hps,dc=lab"
+    # create account name using first and last name
+    sAMAccountName = args[1][0].lower() + args[2].lower()
+    try:
+        connected.add(
+            userdn,
+            attributes={
+                "objectClass": ["organizationalPerson", "person", "top", "user"],
+                "sAMAccountName": sAMAccountName,
+                "userPrincipalName": sAMAccountName + "@hps.lab",
+                "displayName": args[1].capitalize() + " " + args[2].capitalize(),
+            },
+        )
+        # set password
+        connected.extend.microsoft.modify_password(userdn, args[3])
+        # enable user (with password does not expire)
+        connected.modify(userdn, {"userAccountControl": [("MODIFY_REPLACE", 65536)]})
+        # add user to Global Protect Customer Access group
+        addUsersInGroups(connected, userdn, customer_access_groupdn)
+
+        print(f"[+] BOOM! user: {sAMAccountName} created sucessfully!\n")
+
+        choice = template_choice()
+
+        if choice == 0:
+
+            template = create_template(args[1], args[2], args[3])
+
+            if template == 0:
+                print(
+                    """
+[+] Template created successfully! Returning to menu..."""
+                )
+                sleep(2)
+            else:
+                print(
+                    """
+[-] Error making template...returning to menu."""
+                )
+                sleep(2)
+
+        else:
+            return
+
+    except:
+        print("[-] Error! Failed to create user :(")
+        sys.exit(1)
+
+
+def template_choice():
+
+    choice = input(
+        """
+Would you like to create an email template containing details of the user you just created? Press any key to continue or 'N' to skip:
+
+>>> """
+    )
+    if choice == "N":
+        print(
+            """
+[+] Taking you back to the main menu..."""
+        )
+        return 1
+
+    else:
+        return 0
+
+
+def create_template(user_fname, user_lname, user_password):
+
+    user_sAM_account_name = user_fname[0].lower() + user_lname.lower()
+
+    engineer_fname = input(
+        """
+Please enter your first name:
+
+>>> """
+    )
+
+    filename = user_sAM_account_name + "_emailtemp"
+
+    template = {
+        f"""
+Hi {user_fname.capitalize()},
+ 
+Your VPN Credentials are:
+
+{user_sAM_account_name} / {user_password}
+ 
+Please navigate to the HighPoint Solutions GlobalProtect Portal at https://81.145.50.51
+
+You will be prompted to login with your username and password to then download and install the correct version of the Palo Alto GlobalProtect VPN client for your OS.
+ 
+Once you’ve done that you can then login with the same credentials.
+ 
+It’s a split tunnel VPN so only traffic destined for 192.168.200.0/24 will go through the tunnel.
+ 
+If you have any issues connecting please let me know.
+ 
+Thanks,
+
+{engineer_fname.capitalize()}
+
+     """
+    }
+    try:
+        create_file = open(f"{filename}.txt", "w")
+        create_file.writelines(template)
+        create_file.close()
+        return 0
+    except:
+        return
+
+
+def main():
+    menu()
+
+
+if __name__ == "__main__":
+    main()
